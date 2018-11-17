54a65b33b362cb8daa2fcc4dd574aeec2d3c02b6
Jon Rowe <hello@jonrowe.co.uk>
2018-11-17 23:07:07 +0000

    Remove dependency on http

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Elm helpers for working with Jwt tokens.
 
-A collection of functions to decode [Jwt tokens](https://jwt.io/), and to use them for authenticated CRUD requests.
+A collection of functions to decode [Jwt tokens](https://jwt.io/).
 
 ## Decode a token
 
@@ -12,30 +12,10 @@ A Jwt is a Base64 string that has three parts
 
 The library functions `decodeToken` and `tokenDecoder` provide the means to decode the content of a token, while `checkTokenExpiry` and `isExpired` specifically analyse whether the token remains within its expiry time.
 
-## Make an authenticated CRUD request
-
-The library also provides modified versions of Elm standard Http functions to make CRUD requests with the Authorization header set to "bearer <token>"
-
-    let
-        url =
-            "http://example.com/new"
-        body =
-            jsonBody <some Value>
-    in
-        Jwt.post token url body (Json.Decode.field "confirmation" Json.Decode.string)
-
-
 ## Examples
 
 [Examples](https://github.com/simonh1000/elm-jwt/tree/master/examples) are included of the software working with Phoenix and Node backends. More discussion of the Phoenix example can be found in this [blog post](http://simonh1000.github.io/2016/05/phoenix-elm-json-web-tokens/).
 
 ## Changelog
 
-* 6.0.0: 0.19
-* 5.3.0: Adds decoder got Elixir-Guardian token
-* 5.2.0: Update NodeJS example
-* 5.1.0: Adds a decoder for the Firebase Jwt.
-* 5.0.0 (Elm 0.18): Corrects a typo in name of checkTokenExpiry and separates out createRequestObject
-* 4.0.0 (Elm 0.18): Elm's Http library has undergone a major rewrite for 0.18 and this library depends upon it. As a result much has changed and you are encouraged to re-look at the examples and the [docs](http://package.elm-lang.org/packages/simonh1000/elm-jwt/latest/Jwt).
-* 3.0.0 (Elm 0.18): Elm 0.17 users should use version 2.0.0.
-* 2.0.0 (Elm 0.17): The one breaking change is that authenticate now returns `Task JwtError String` rather than `Task never (Result JwtError String)`. It is better to leave it to the user to handle the conversion to a Cmd. Elm 0.16 users should use version 1.0.2.
+* 1.0.0: Fork from simonh1000/elm-jwt to remove the dependency on elm/http
--- src/Jwt.elm
+++ src/Jwt.elm
@@ -2,18 +2,8 @@ module Jwt
     exposing
         ( JwtError(..)
         , checkTokenExpiry
-        , createRequest
-        , createRequestObject
         , decodeToken
-        , delete
-        , get
-        , handleError
         , isExpired
-        , post
-        , promote401
-        , put
-        , send
-        , sendCheckExpired
         , tokenDecoder
         )
 
@@ -28,19 +18,13 @@ authenticated Http requests.
 @docs decodeToken, tokenDecoder, isExpired, checkTokenExpiry
 
 
-# Authenticated Http requests
-
-@docs createRequest, createRequestObject, send, sendCheckExpired, get, post, put, delete
-
-
 # Error handlers
 
-@docs JwtError, promote401, handleError
+@docs JwtError
 
 -}
 
 import Base64
-import Http exposing (Request, expectJson, header, jsonBody, request, toTask)
 import Json.Decode as Decode exposing (Decoder, Value, field)
 import String
 import Task exposing (Task)
@@ -62,7 +46,6 @@ type JwtError
     | TokenNotExpired
     | TokenProcessingError String
     | TokenDecodeError Decode.Error
-    | HttpError Http.Error
 
 
 errorToString : JwtError -> String
@@ -203,149 +186,3 @@ checkUnacceptedToken token now =
         Err jwtErr ->
             -- Pass through a decoding error
             jwtErr
-
-
-
-{- ====================================================
-    MAKING AUTHENTICATED API CALLS
-   ====================================================
--}
-
-
-{-| createRequest creates a Http.Request with the token added to the headers, and
-sets the `withCredentials` field to True.
--}
-createRequest : String -> String -> String -> Http.Body -> Decoder a -> Http.Request a
-createRequest method token url body =
-    createRequestObject method token url body >> request
-
-
-{-| createRequestObject creates the data structure expected by Http.Request.
-It is broken out here so that users can change the expect part in the event that
-one of their REST apis does not return Json.
-
-In my experience, the Authorization header is NOT case sensitive. Do raise an issue if you experience otherwise.
-
-See [MDN](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/withCredentials) for more on withCredentials. The default is False.
-
--}
-createRequestObject :
-    String
-    -> String
-    -> String
-    -> Http.Body
-    -> Decoder a
-    -> { method : String, headers : List Http.Header, url : String, body : Http.Body, expect : Http.Expect a, timeout : Maybe Float, withCredentials : Bool }
-createRequestObject method token url body dec =
-    { method = method
-    , headers = [ header "Authorization" ("Bearer " ++ token) ]
-    , url = url
-    , body = body
-    , expect = expectJson dec
-    , timeout = Nothing
-    , withCredentials = False
-    }
-
-
-{-| `get` is a replacement for `Http.get` that returns a Http.Request with the token
-attached to the headers.
-
-    getData : String -> Cmd Msg
-    getData token =
-        Jwt.get token "/api/data" dataDecoder
-            |> Jwt.send DataResult
-
--}
-get : String -> String -> Decoder a -> Request a
-get token url dec =
-    createRequest "GET" token url Http.emptyBody dec
-
-
-{-| post is a replacement for `Http.post` that returns a Http.Request with the token
-attached to the headers.
-
-** Note that is important to use jsonBody to ensure that the 'application/json' is added to the headers **
-
-    postContent : Token -> Decoder a -> E.Value -> String -> Request a
-    postContent token dec value url =
-        Jwt.post token url (Http.jsonBody value) (phoenixDecoder dec)
-            |> Jwt.send ContentResult
-
--}
-post : String -> String -> Http.Body -> Decoder a -> Request a
-post =
-    createRequest "POST"
-
-
-{-| Create a PUT request with a token attached to the Authorization header
--}
-put : String -> String -> Http.Body -> Decoder a -> Request a
-put =
-    createRequest "PUT"
-
-
-{-| returns a `DELETE` Http.Request with the token attached to the headers.
--}
-delete : String -> String -> Decoder a -> Request a
-delete token url dec =
-    createRequest "DELETE" token url Http.emptyBody dec
-
-
-{-| `send` replaces `Http.send`. On receipt of a 401 error, it returns a Jwt.Unauthorized.
--}
-send : (Result JwtError a -> msg) -> Request a -> Cmd msg
-send msgCreator req =
-    let
-        conv : (Result JwtError a -> msg) -> (Result Http.Error a -> msg)
-        conv fn =
-            fn << Result.mapError promote401
-    in
-        Http.send (conv msgCreator) req
-
-
-{-| `sendCheckExpired` is similar to `send` but, on receiving a 401, it carries out a further check to
-determine whether the token has expired.
--}
-sendCheckExpired : String -> (Result JwtError a -> msg) -> Request a -> Cmd msg
-sendCheckExpired token msgCreator req =
-    req
-        |> toTask
-        |> Task.map Result.Ok
-        |> Task.onError (Task.map Err << handleError token)
-        |> Task.perform msgCreator
-
-
-{-| Takes an Http.Error. If it is a 401, check the token for expiry.
--}
-handleError : String -> Http.Error -> Task Never JwtError
-handleError token err =
-    case promote401 err of
-        Unauthorized ->
-            checkTokenExpiry token
-
-        other ->
-            Task.succeed other
-
-
-{-| Examines a 401 Unauthorized reponse, and converts the error to TokenExpired
-when that is the case.
-
-    getAuth : String -> String -> Decoder a -> Task Never (Result JwtError a)
-    getAuth token url dec =
-        createRequest "GET" token url Http.emptyBody dec
-            |> toTask
-            |> Task.map Result.Ok
-            |> Task.onError (promote401 token)
-
--}
-promote401 : Http.Error -> JwtError
-promote401 err =
-    case err of
-        Http.BadStatus { status } ->
-            if status.code == 401 then
-                Unauthorized
-            else
-                HttpError err
-
-        _ ->
-            HttpError err
